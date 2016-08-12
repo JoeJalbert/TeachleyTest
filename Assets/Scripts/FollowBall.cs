0641fbf44844d1c166fbb75af87c684a8365f614
JoeJalbert <joejalbert29@gmail.com>
2016-08-12 13:35:59 -0400

    Adjusted speed values for the following camera.

--- Assets/Scripts/FollowBall.cs
+++ Assets/Scripts/FollowBall.cs
@@ -6,26 +6,30 @@ public class FollowBall : MonoBehaviour {
     public Transform Ball;
     public float followSpeed;
 
+    public float topSpeed;
+    public float midSpeed;
+    public float lowSpeed;
+
     void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Ball.position, followSpeed / Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, Ball.position, followSpeed * Time.deltaTime);
         Vector3 tempPos = transform.position;
         tempPos.z = -10;
         transform.position = tempPos;
 
         float distance = Vector2.Distance(transform.position, Ball.position);
         // Increase the speed if the ball is getting too far away.
-        if (distance > 1)
+        if (distance > .8f)
         {
-            followSpeed = Mathf.Lerp(followSpeed, .002f, .003f / Time.deltaTime);
+            followSpeed = Mathf.Lerp(followSpeed, topSpeed, 3f * Time.deltaTime);
         }
         else if(distance > .2f)
         {
-            followSpeed = Mathf.Lerp(followSpeed, .0004f, .003f / Time.deltaTime);
+            followSpeed = Mathf.Lerp(followSpeed, midSpeed, 3f * Time.deltaTime);
         }
         else
         {
-            followSpeed = .0001f;
+            followSpeed = lowSpeed;
         }
     }
 
